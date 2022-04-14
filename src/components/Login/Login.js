a9c6e55337ad2a73547618bf97ae5b3caf55d099
niloyds <niloyds2004@gmail.com>
2022-04-14 15:40:41 +0600

    SignIn and SignOut using auth and hooks

---
+++ src/components/Login/Login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
+
+const Login = () => {
+    const {signInWithGoogle} = useFirebase();
+    return (
+        <div>
+            <h3>Please Login</h3>
+            <div style={{margin: '20px'}}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form>
+                <input type="email" placeholder='Your email' />
+                <br />
+                <input type="password" name="" id="" placeholder='Enter Password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZPRge73SLveuxajOqLzewI_bKqxt_tKo",
+  authDomain: "router-firebase-integrat-e3c62.firebaseapp.com",
+  projectId: "router-firebase-integrat-e3c62",
+  storageBucket: "router-firebase-integrat-e3c62.appspot.com",
+  messagingSenderId: "655677519222",
+  appId: "1:655677519222:web:f7f398c63d333f81a93657"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
